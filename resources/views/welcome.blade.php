5580a4e0b0dc4c47f13013fc5a9824e878c4cb11
Cess de Vera <louryne.devera@yahoo.com>
2018-05-29 17:25:36 +0800

    added link to new post for logged user

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -70,6 +70,7 @@
                 <div class="top-right links">
                     @auth
                         <a href="{{ url('/home') }}">Home</a>
+                        <a href="{{ url('/new') }}">New Post</a>
                     @else
                         <a href="{{ route('login') }}">Login</a>
                         <a href="{{ route('register') }}">Register</a>
