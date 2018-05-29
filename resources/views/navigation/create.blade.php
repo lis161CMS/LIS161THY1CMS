13d5658addac5d54072915da85176c1175d7b4e9
ella <ellamae_abela@yahoo.com>
2018-05-30 00:24:54 +0800

    Edited create

--- resources/views/navigation/create.blade.php
+++ resources/views/navigation/create.blade.php
@@ -3,9 +3,25 @@
 @section('content')
 <h3>Select navigation links</h3>
 <form action="{!! URL::to('navigation')!!}" method="post">
+	{{ csrf_field() }}
 	<input type="checkbox" name="navigationName[]" value="Home">Home</input>
 	<input type="checkbox" name="navigationName[]" value="Link1">Link1</input>
 	<input type="checkbox" name="navigationName[]" value="Link1">Link2</input>
 	<input type="submit" name="submit" value="Submit"/>
 </form>
+
+<?php
+	function IsSelected($name,$value){
+		foreach($_POST[$name] as $checkvalue)
+		{
+			if($checkvalue == $value){
+				return true;
+			}
+		}
+	}
+
+	if(IsSelected('navigationName', 'Home')){
+		<input type="hidden" name="navigationLink[]" value="Home">
+	}
+?>
 @endsection
