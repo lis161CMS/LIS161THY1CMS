c285662377f89c0de977e69bd0ea9c0158b33c5f
jastined <34388120+jastined@users.noreply.github.com>
2018-05-29 16:20:27 +0800

    Update create.blade.php

--- resources/views/navtype/create.blade.php
+++ resources/views/navtype/create.blade.php
@@ -1,9 +1,17 @@
 @extends('home')
 
 @section('content')
+<form method="post" action="{{url('navtype')}}">
 	<h3> Enter desired navigation type</h3>
 	<div class="form-group">
 		{{ csrf_field() }}
 		<input type="text" name="navigationType" placeholder="Sidebar or Topbar" />
 	</div>
+	<h3> Enter your user ID</h3>
+	<div class="form-group">
+		<input type="text" name="user" placeholder="1 or 2" />
+	</div>
+	<input type="submit" />
+</form>
+
 @endsection
