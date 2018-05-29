91e238bc38c03ca10590d7d4ab62eaa5a1ed3e3e
jastined <34388120+jastined@users.noreply.github.com>
2018-05-29 17:22:20 +0800

    Radio button

--- resources/views/navtype/create.blade.php
+++ resources/views/navtype/create.blade.php
@@ -5,11 +5,13 @@
 	<h3> Enter desired navigation type</h3>
 	<div class="form-group">
 		{{ csrf_field() }}
-		<input type="text" name="navigationType" placeholder="Sidebar or Topbar" />
+		<input type="radio" name="navigationType" value="sidebar">Sidebar
+		<input type="radio" name="navigationType" value="topbar">Topbar
 	</div>
-	<h3> Enter your user ID</h3>
+	<h3> Choose which user has this navigation type</h3>
 	<div class="form-group">
-		<input type="text" name="user" placeholder="1 or 2" />
+		<input type="radio" name="user" value="1">Guest
+		<input type="radio" name="user" value="2">Admin
 	</div>
 	<input type="submit" />
 </form>
