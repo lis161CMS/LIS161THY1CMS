9c97efc7643499133c15ec41eea2246d412f0751
Jeanne Torres <torresjeannedenyse@yahoo.com.ph>
2018-05-30 20:54:03 +0800

    Edited navigations.create

--- resources/views/navigation/create.blade.php
+++ resources/views/navigation/create.blade.php
@@ -1,15 +1,30 @@
 @extends('layouts.app')
 
 @section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Navigations for User</div>
 
-<h3>Navigations for User</h3>
-<br/><br/>
-{{ Form::open(array('action' => 'NavigationController@navupdate')) }}
-{{ csrf_field() }}
-@foreach($navs as $nav)
-	{{ Form::checkbox($nav->navigationLink, $nav->id, $nav->navactivated) }} {{ $nav->navigationName }}
-@endforeach
-{{ Form::submit() }}
-{{ Form::close() }}
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success">
+                            {{ session('status') }}
+                        </div>
+                    @endif
 
+                    {{ Form::open(array('action' => 'NavigationController@navupdate')) }}
+					{{ csrf_field() }}
+					@foreach($navs as $nav)
+						{{ Form::checkbox($nav->navigationLink, $nav->id, $nav->navactivated) }} {{ $nav->navigationName }}<br/>
+					@endforeach
+					<br/>
+					{{ Form::submit() }}
+					{{ Form::close() }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
 @endsection
