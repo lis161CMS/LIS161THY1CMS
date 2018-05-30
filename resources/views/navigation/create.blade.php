3cc7598526faf1e11493ccd8dde8b1d10e267a50
ella <ellamae_abela@yahoo.com>
2018-05-30 13:36:38 +0800

    Updated navigation

--- resources/views/navigation/create.blade.php
+++ resources/views/navigation/create.blade.php
@@ -4,10 +4,10 @@
 
 <h3>Navigations for User</h3>
 <br/><br/>
-{{ Form::open(array('action' => 'NavigationController@myupdate')) }}
+{{ Form::open(array('action' => 'NavigationController@navupdate')) }}
 {{ csrf_field() }}
 @foreach($navs as $nav)
-	{{ Form::checkbox($nav->navigationLink, $nav->id, $nav->navactivated) }} {{ $nav->navigationName }}<br/>
+	{{ Form::checkbox($nav->navigationLink, $nav->id, $nav->navactivated) }} {{ $nav->navigationName }}
 @endforeach
 {{ Form::submit() }}
 {{ Form::close() }}
