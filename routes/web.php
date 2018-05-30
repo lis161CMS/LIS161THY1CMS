6b33add88556663a398ec28d7dbbd78e3a139417
Gynard Alfonso <39150962+MaNia-Innocence@users.noreply.github.com>
2018-05-30 18:16:27 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('users', 'UserController');
+
 Route::resource('navtype', 'NavTypeController');
 
 Route::post('navigation.navupdate', 'NavigationController@navupdate');
