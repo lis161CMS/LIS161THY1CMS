cd820f80cac2af0a583ba3c96417ff07819396b8
jastined <34388120+jastined@users.noreply.github.com>
2018-05-31 10:08:32 +0800

    Updated form

--- resources/views/navtype/create.blade.php
+++ resources/views/navtype/create.blade.php
@@ -1,6 +1,11 @@
 @extends('layouts.nav')
 
 @section('content')
+<div class="container">
+	<div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+            	<div class="card-body">
 <form method="post" action="{{url('navtype')}}">
 	<h3> Enter desired navigation type</h3>
 	<div class="form-group">
@@ -8,12 +13,16 @@
 		<input type="radio" name="navigationType" value="sidebar">Sidebar
 		<input type="radio" name="navigationType" value="topbar">Topbar
 	</div>
-	<h3> Choose which user has this navigation type</h3>
+	<h3> Enter your first name</h3>
 	<div class="form-group">
-		<input type="radio" name="user" value="1">Guest
-		<input type="radio" name="user" value="2">Admin
+		<input type="text" name="name"/>
 	</div>
 	<input type="submit" />
 </form>
+</div>
+</div>
+</div>
+</div>
+</div>
 
 @endsection
